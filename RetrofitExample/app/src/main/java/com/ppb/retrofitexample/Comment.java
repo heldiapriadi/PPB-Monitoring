614563d9e73b7488e0d505eb26041fe3d5ec6735
heldiapriadi <heldiapriadi@gmail.com>
2021-07-12 09:10:57 +0700

    maps & api

---
+++ RetrofitExample/app/src/main/java/com/ppb/retrofitexample/Comment.java
@@ -0,0 +1,28 @@
+package com.ppb.retrofitexample;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    @SerializedName("body")
+    private String text;
+    public int getPostId() {
+        return postId;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getText() {
+        return text;
+    }
+}
---
+++ RetrofitExample/app/src/main/java/com/ppb/retrofitexample/Post.java
@@ -0,0 +1,36 @@
+package com.ppb.retrofitexample;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+
+    private Integer  id;
+
+    private String title;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
+    @SerializedName("body")
+    private String text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
